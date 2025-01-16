a992db2b4b511ab64209c7e949e80db62f9c6e12
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2025-01-16 10:40:11 +0100

    add Header and SearchBar components; implement trendingMovies data structure

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,16 @@
+import SearchBar from "./SearchBar";
 
 const Header = () => {
   return (
-    <div>
-        header
-    </div>
-  )
-}
+    <header className="bg-gray-900 py-6 px-4 shadow-md">
+      <div className="container mx-auto flex flex-col md:flex-row items-center justify-between">
+        <h1 className="text-white text-2xl font-bold mb-4 md:mb-0">MovieApp</h1>
+        <div className="w-full md:w-1/2">
+          <SearchBar />
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
