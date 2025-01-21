655b955e4955dfffb94cc7d85b44f76e7b540c03
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2025-01-21 10:12:20 +0100

    enhance MovieCard component; improve styling with background, padding, and rounded corners

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,7 +3,7 @@
 const MovieCard = ({ movie }) => {
   console.log(movie);
   return (
-    <div className="flex flex-col gap-4 hover:cursor-pointer">
+    <div className="flex flex-col gap-4 hover:cursor-pointer bg-slate-100/10 p-5 rounded-lg">
       <img src={movie.Poster} className="h-64 rounded-xl" alt="" />
       <div className="flex flex-col text-sm">
         <h2 className=" font-bold">{movie.Title}</h2>
